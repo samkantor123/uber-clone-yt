5c8128fae72f0d72502b1132cbcc89bb111492af
samkantor123 <sk2467@cornell.edu>
2023-02-26 17:49:07 -0800

    began work on homescreen

--- App.js
+++ App.js
@@ -1,12 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import {Provider} from "react-redux";
+import { store } from "./store";
+import HomeScreen from './screens/HomeScreen';
+
+//1) Set up Redux (a data layer sorrounding app)
+
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+      <HomeScreen />
+    </Provider>
   );
 }
 
