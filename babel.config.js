3218bf85dca88a3c8a7a2522881fb9b7211f1a3c
samkantor123 <sk2467@cornell.edu>
2023-02-28 01:28:10 -0800

    set up google api keys

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,14 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        "module:react-native-dotenv",
+        {
+          moduleMame: "@env",
+          path: ".env",
+        },
+      ],
+    ],
   };
 };
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -3,6 +3,10 @@ import React from 'react'
 import tw from 'tailwind-react-native-classnames';
 
 import NavOptions from '../components/NavOptions';
+import {GooglePlacesAutocomplete } from
+'react-native-google-places-autocomplete';
+import {GOOGLE_MAPS_APIKEY} from "@env";
+
 
 const HomeScreen = () => {
   return (
@@ -18,8 +22,16 @@ const HomeScreen = () => {
                 uri: "https://links.papareact.com/gzs",
             }}
         />
+
+        <GooglePlacesAutocomplete
+          nearbyPlacesAPI="GooglePlacesSearch"
+          debounce={400}
+          placeholder="Where From?"
+        />
+
         <NavOptions />
       </View>
+
     </SafeAreaView>
   )
 }
